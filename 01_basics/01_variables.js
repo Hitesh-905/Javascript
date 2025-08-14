87165efcb81616c6d20360fc27a913702be5117b
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-08-14 06:07:11 +0000

    additional information

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,3 +8,13 @@ var accpassword="15892";
 
 console.table([accountNumber,accpassword,accName]);
 
+// we cannot update const keyword 
+// let is an block scoped variable
+// var is an global scoped variable 
+
+//const accountNumber=896632;    // this will throw an error as we cannot reassign a const variable
+let acccity="Delhi"; // this is allowed as let can be reassigned
+accName="Hitesh"; // this is allowed as let can be reassigned
+
+
+var accState="UP"; // this is allowed as var can be reassigned
