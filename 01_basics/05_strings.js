a96586928baf16adb6d0b1baaec0147e5eaab506
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-08-16 04:38:38 +0000

    learned about string functions

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+// strings
+
+const stringname="hello world";
+const repocount =2;
+
+// console.log(`the name of my string is ${stringname} and the repo count is ${repocount}`); //template literals
+
+const newname =new String("hello world");//creating a string object 
+
+// console.log(newname.toUpperCase()); //HELLO WORLD
+
+// console.log(newname.charAt(6));
+// console.log(newname.__proto__);
+
+//lets lear about more methods in string object such as slice , substring , trim , replace
+const newstring= newname.substring(0,5); //substring method extracts characters from index 0 to index 5 (not including 5)
+// console.log(newstring);
+
+console.log(newname.slice(-9,4));
+
+const nameofuser="   hiteshChoudhary  ";
+console.log(nameofuser);
+console.log(nameofuser.trim());//trims the white spaces from both the ends i,it only works for white spaces not or other characters
+
+const url="https://hiteshchoudhary/courses%20javascript.com";
+console.log(url.replace("%20", "-")); //replaces the first occurrence of %20 with a -
+
+//another exapme of string functions is split which splits the string into an array of substrings based on specific seperator or character
+const sentence="hello channel welcome to my guys";
+console.log(sentence.split(" ")); //splits the string into an array of substrings based on space character
+
