92044085c7323415475ef5dc7ab9965fed1b70bb
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-08-17 11:49:23 +0000

    njn

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,4 @@
+//the most important thing to remember is that objects are mutable`
+// the most importamt thing to master in javascript is objects and events
+
+// we can create an object by literal or constructor method
