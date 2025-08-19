1facea9adeb77e78928a4933b6e86533710f40eb
Hitesh-905 <choudharyhitesh905@gmail.com>
2025-08-19 14:01:25 +0000

    scope info

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,22 @@
+//lets learn about the scope of an variable
+
+
+
+if(true){
+   const a =5;
+let b = 6;
+var c = 8 ;
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// number(5);
+function number(num){
+  console.log(num + 1);
+}
+add(5);
+const add=function numberTwo(numberlelo){
+      console.log(numberlelo + 2)
+}
+
